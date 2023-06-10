c572c1d6830f932190bf5a7f12714380a1945e7a
alinstanimir <alin.stanimir@gmail.com>
2023-06-10 09:36:53 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Success from "./pages/Success";
 
